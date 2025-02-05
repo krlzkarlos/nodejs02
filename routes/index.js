4db108e9361a9428b80b0e308e633e20c64330e4
Carlos Gamarra <gamarracf@icloud.com>
2025-02-04 21:36:11 -0500

    Agregando ruta /api/v1

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,19 @@
+const express = require('express');
+
 const productsRouter = require('./products.router');
 const usersRouter = require('./users.router');
 const categoriesRouter = require('./categories.router');
 const alamacenesRouter = require('./almacenes.router');
 
 function routerApi(app) {
-  app.use('/products', productsRouter);
-  app.use('/users', usersRouter);
-  app.use('/categories', categoriesRouter);
-  app.use('/almacenes', alamacenesRouter);
+  // aplicando la ruta Api V1
+  const router = express.Router();
+  app.use('/api/v1', router);
+
+  router.use('/products', productsRouter);
+  router.use('/users', usersRouter);
+  router.use('/categories', categoriesRouter);
+  router.use('/almacenes', alamacenesRouter);
 }
 
 module.exports = routerApi;
