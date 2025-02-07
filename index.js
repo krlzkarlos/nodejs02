f94ee05947e78941e460cdbb149b9a2e4542e0a8
Carlos Gamarra <gamarracf@icloud.com>
2025-02-06 19:10:21 -0500

    GET,POST,PUT,PATCH,DELTE

--- index.js
+++ index.js
@@ -2,42 +2,14 @@ const express = require('express');
 const routerApi = require('./routes/');
 
 const app = express();
-const port = 3000;
-const { faker } = require('@faker-js/faker');
+const port = 3500;
+
+//Middleware
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hola mi server en expres');
 });
-
-app.get('/nueva-ruta', (req, res) => {
-  res.send('hola soy una nueva ruta');
-});
-
-// app.get('/users', (req, res) => {
-//   const { limit, offset } = req.query;
-
-//   if (limit && offset) {
-//     res.json([
-//       {
-//         limit,
-//         offset,
-//       },
-//     ]);
-//   } else {
-//     res.send('No hay parametros');
-//   }
-// });
-
-// app.get('/categories/:categoryId/products/:productId', (req, res) => {
-//   const { categoryId, productId } = req.params;
-
-//   res.json([
-//     {
-//       categoryId,
-//       productId,
-//     },
-//   ]);
-// });
 
 app.listen(port, () => {
   console.log('mi port  backEnd GamaroServer ' + port);
--- routes/categories.router.js
+++ routes/categories.router.js
@@ -3,6 +3,52 @@ const router = express.Router();
 
 const { faker } = require('@faker-js/faker');
 
+router.get('/', (req, res) => {
+  const categories = [];
+
+  for (let index = 0; index < 100; index++) {
+    categories.push({
+      name: faker.company.name(),
+      type: faker.commerce.department(),
+    });
+  }
+
+  res.json(categories);
+});
+
+router.post('/', (req, res) => {
+  const body = req.body;
+
+  res.json([
+    {
+      message: 'Created',
+      data: body,
+    },
+  ]);
+});
+
+router.patch('/:id', (req, res) => {
+  const { id } = req.params;
+
+  res.json([
+    {
+      message: 'Update',
+      id: id,
+    },
+  ]);
+});
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+
+  res.json([
+    {
+      message: 'Delete',
+      id: id,
+    },
+  ]);
+});
+
 router.get('/:categoryId/products/:productId', (req, res) => {
   const { categoryId, productId } = req.params;
 
